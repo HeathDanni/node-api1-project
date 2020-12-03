3babf3061ca36e6cec51ae6448625f720e992e4a
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-03 16:49:18 -0700

    added bio to users

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: "1", name: "Jane Doe", bio: "Likes kittens"},
+	{ id: "2", name: "John Doe", bio: "Dislikes kittens"},
+	{ id: "3", name: "Jack Doe", bio: "What's a kitten?"},
 ]
 
 function getUsers() {
